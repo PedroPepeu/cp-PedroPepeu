88c577500114141a8b23e29dbf5817962325c408
Freitas Pepeu <pedrosteamgtav@gmail.com>
2023-04-21 19:23:09 -0300

    codeforces

---
+++ codeforces/WrongSubtraction/Wrong.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <bitset>
+
+using namespace std;
+
+int n, k;
+
+int main() {
+    cin >> n;
+    cin >> k;
+    while(k--) {
+        bitset<8> b(n);
+        if() {
+            n--;
+        } else {
+            n /= 10;
+        }
+    }
+    cout << n << endl;
+}
