94b041573b8ec368aa9afc86a14b39814a628fce
Freitas Pepeu <pedrosteamgtav@gmail.com>
2023-04-23 20:13:37 -0300

    Im not ranging all the possible cases

--- Neps/HelicopterFugitive/Heli.cpp
+++ Neps/HelicopterFugitive/Heli.cpp
@@ -3,11 +3,20 @@
 using namespace std;
 
 int main() {
-    int h, p, f, d;
-    cin >> h >> p >> f >> d;
+    int H, P, F, d;
+    cin >> H >> P >> F >> d;
+
     if(d == -1) {
-    
+        if(H < F && F < P) cout << "S" << endl;
+        else if(F < H && H < P) cout << "N" << endl;
+        else if(F < P && P < H) cout << "S" << endl;
+        else if(H < P && P < F) cout << "N" << endl;
     } else {
-    
+        if(H < F && F < P) cout << "N" << endl;
+        else if(F < H && H < P) cout << "S" << endl;
+        else if(F < P && P < H) cout << "N" << endl;
+        else if(H < P && P < F) cout << "S" << endl;
     }
+
+    return 0;
 }
