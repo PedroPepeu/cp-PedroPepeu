efd3723d9c66de61a33c7a4cd3532b2bf5a64e5b
PedroPepeu <pedrosteamgtav@gmail.com>
2023-05-30 18:02:43 -0300

    team training

--- codeforces/TeamTraining/AB.cpp
+++ codeforces/TeamTraining/AB.cpp
@@ -6,13 +6,29 @@ int main() {
     int a, b;
     cin >> a >> b;
 
-    int teams;
-
-    if(a > b) {
-        teams = a / 2;
-    } else {
-        teams = (b+1) / 2;
+    int teams = 0;
+    
+    if(b > a) {
+        int temp = b;
+        b = a;
+        a = temp;
     }
 
-    cout << teams << endl;
+    if((b * 2) <= a) {
+        cout << b << endl;
+    } else {
+        int i;
+        for(i = 0; a > 0 && b > 0 && a + b >= 3; i++) {
+            if(a > b) {
+                a-=2;
+                b--;
+            } else {
+                a--;
+                b-=2;
+            }
+        }
+        cout << i << endl;
+    }
+
+    return 0;
 }
